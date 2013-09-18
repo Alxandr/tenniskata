8109fc607015eafe3bee2ef1833a5acc5a851ac4
Alxandr <alxandr@alxandr.me>
2013-09-18 17:57:08 +0200

    Small test changes

--- spec/TennisSpec.js
+++ spec/TennisSpec.js
@@ -5,12 +5,12 @@ describe('Tennis', function () {
 		tennis = new Tennis();
 	});
 
-	it("at beginning of game", function () {
+	it("should return \"Love - Love\" at beginning of game", function () {
 		expect(tennis.getScore()).toBe("Love - Love");
 	});
 
-	it("first match", function () {
+	it("should return \"Fifteen - Love\" if first player takes first point", function () {
 		tennis.wonPoint("player1");
-		expect(tennis.getScore()).toBe("...");
+		expect(tennis.getScore()).toBe("Fifteen - Love");
 	});
 });
