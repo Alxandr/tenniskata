1a73df06c983061d7da6edb1aa3d70a775090f08
simensma <simensma@gmail.com>
2013-09-18 18:04:32 +0200

    More changes

--- src/Tennis.js
+++ src/Tennis.js
@@ -1,10 +1,22 @@
+
 function Tennis() {
+	this.score=["Love","Fifteen"];
+	this.player1Score= 0;
+	this.player2Score= 0;
 }
 
+
+
 Tennis.prototype.wonPoint = function(player) {
+	if(player==="player1"){
+		this.player1Score++;
+	}else{
+		this.player2Score++;
+	}
 
 };
 
 Tennis.prototype.getScore = function() {
-	return "?";
+
+	return this.score[this.player1Score] +" - "+this.score[this.player2Score];
 };
